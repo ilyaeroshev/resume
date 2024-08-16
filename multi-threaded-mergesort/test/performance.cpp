48ebfb0c897f19c19b4ce082312cc7364685ec95
Ilya Eroshev <ilyaeroshev@MacBook-Air-Ilya.local>
2024-08-16 10:31:58 +0300

    add new performance test

--- multi-threaded-mergesort/test/performance.cpp
+++ multi-threaded-mergesort/test/performance.cpp
@@ -35,8 +35,40 @@ void Test1() {
   std::cerr << "Test #1 was completed in " << all_time << std::endl;
 }
 
+void Test2() {
+  size_t seed = std::time(nullptr);
+  std::cerr << "Test #2 seed = " << seed << '\n';
+  std::srand(seed);
+
+  size_t workers = std::max(1u, std::thread::hardware_concurrency());
+  std::cerr << "Test #2 workers = " << workers << '\n';
+  ThreadPool tp(workers);
+
+  tp.Start();
+
+  const size_t TESTS_NUMBER = 5;
+
+  std::chrono::milliseconds all_time{0};
+
+  for (size_t test = 0; test < TESTS_NUMBER; ++test) {
+    const size_t VSZ = 1'000'000;
+    const size_t SSZ = 1000;
+
+    auto data = GetRandomStringVector(VSZ, SSZ);
+
+    Timer timer;
+    MergeSort(all(data), static_cast<IScheduler&>(tp));
+    all_time += timer.Spent<std::chrono::milliseconds>();
+  }
+
+  tp.Stop();
+
+  std::cerr << "Test #2 was completed in " << all_time << std::endl;
+}
+
 int main() {
   Test1();
+  Test2();
 
   return 0;
 }
--- single-threaded-mergesort/test/utility.hpp
+++ single-threaded-mergesort/test/utility.hpp
@@ -34,10 +34,37 @@ void Check(ForwardIt begin, ForwardIt end) {
 
 std::vector<int> GetRandomVector(size_t N) {
   std::vector<int> res;
-  res.reserve(N);
+  res.resize(N);
 
-  for (size_t i = 0; i < N; ++i) {
-    res.push_back(std::rand() % 1'000'000 - 500'000);
+  for (int& t: res) {
+    t = std::rand() % 1'000'000 - 500'000;
+  }
+
+  return res;
+}
+
+std::string GetRandomString(size_t N) {
+  std::string res;
+  res.resize(N);
+
+  for(char& c: res) {
+    c = 'a' + std::rand() % 26;
+  }
+
+  return res;
+}
+
+std::vector<std::string> GetRandomStringVector(size_t VSZ, size_t SSZ) {
+  std::vector<std::string> res(VSZ);
+
+  const size_t RANDOM_STRINGS = 1000;
+
+  for(size_t i = 0; i < std::min(RANDOM_STRINGS, VSZ); ++i) {
+    res[i] = GetRandomString(SSZ);
+  }
+
+  for(size_t i = RANDOM_STRINGS; i < VSZ; ++i) {
+    res[i] = res[std::rand() % RANDOM_STRINGS];
   }
 
   return res;
